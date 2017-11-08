b1b324cf994e3c048ca0a392ca48af25e070cdd3
iamdevonbutler <jpescione@gmail.com>
2017-11-08 14:28:16 -0500

    in progress...

--- perf/index.js
+++ perf/index.js
@@ -1,42 +1,19 @@
-// @todo test using actual array buffers over string buffer hack
-// @todo gotta do arrays and objects.
 const microtime = require('microtime');
 
-const jsonf = require('../lib');
+const {deconstruct, reconstruct} = require('../lib');
 
 const st = microtime.now();
 var n = 1000000;
 var i = n;
 var str = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet nobis dignissimos, quas! Est asperiores ab beatae ex, dolor optio nostrum rerum aspernatur in corporis unde aperiam error fuga ut, illo?';
 
-var obj = {
-  // a: 1,
-  // b: {
-    // c: 'string',
-    // d: [111,222,333],
-  // },
-  // e: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, labore adipisci accusantium! Iure, corporis quia numquam ea error nam sunt. Labore eveniet, nobis eos tempore quis non numquam incidunt nisi!',
-  f: {},
-  g: [],
-  h: function*() {
 
-    console.log(1);
-  },
-  // h(a,b) { console.log(1); },
-  // u: () => {
-
-  // },
-  // i: '',
-  // j: true,
-  // k: false,
-  // l: null,
-  // m: undefined,
-  // n: NaN,
-};
-
-// @todo empty function
-// @todo does this support classes inside of functions?
+// async
 //
+//
+
+// @todo does this support classes inside of functions?
+
 // function() {}
 // function*() {}
 // function name() {}
@@ -49,35 +26,31 @@ var obj = {
 // }
 // var name = function() {};
 // var name = function*() {};
-// var obj = [111,222,333, {a: 1}, [1,2]];
+
 
 // @todo default value for func params
 // @todo function params are functions
 // @todo unit test deconstructFunction needs it.
-
-obj = {
-  name: function aaa(param, param1 = null) {console.log(44);},
-  name0: function() {console.log(1);},
-  name1: () => {console.log(1);},
-  // name2: function* () {},
-  name2: function* aaa () {},
-  name3() {console.log(1);},
-  name4: async function() {console.log(1);},
-  name4: async function aaa() {console.log(1);},
-  name5: async () => {console.log(1);},
-};
-
 // @todo make sure recursion works. how to call self. make a note in the docs.
-var str = jsonf.stringify(obj);
-var obj = jsonf.parse(str);
+
+// obj = {
+//   function aaa(param, param1 = null) {console.log(44);},
+//   function() {console.log(1);},
+//   () => {console.log(1);},
+//   function* () {},
+//   function* aaa () {},
+//   name3() {console.log(1);},
+//   async function() {console.log(1);},
+//   async function aaa() {console.log(1);},
+//   async () => {console.log(1);},
+// };
+
 console.log(obj);
 return;
+
 // while (i--) {
 //   var x = 1;
 // }
-
-
-
 
 const ms = (microtime.now() - st) / 1000;
 
--- tests/main.test.js
+++ tests/main.test.js
@@ -1,139 +1,312 @@
 const should = require('chai').should();
 const expect = require('chai').expect;
 const assert = require('chai').assert;
+// @todo exclude tests where generators are async.
+
+class Possibilities {
+
+  constructor(...args) {
+    this._preprocessors = [];
+    this._args = args;
+    this._map = args.map(arg => this._fact(arg.length));
+    this._matrix = this._buildMatrix();
+  }
+
+  _fact(num) {
+    var obj = [];
+    if (num > 0) {
+      let i = 0;
+      while (num--) {
+        obj.push(i);
+        i++;
+      }
+      return obj;
+    }
+    return false;
+  }
+
+  _buildMatrix(i = 0) {
+    var result = [];
+    this._map[i].forEach(data => {
+      if (i < this._map.length - 1) {
+        let obj = this._buildMatrix(i + 1);
+        obj.forEach(item => result.push([data, ...item]));
+      }
+      else {
+        result.push(this._map[i]);
+      }
+    });
+    return result;
+  }
+
+  preprocess(obj) {
+    this._preprocessors.push(obj);
+  }
+
+  run() {
+    return this._matrix.map(data => {
+      return data.map((item, i) => {
+        return this._args[i][item];
+      });
+    });
+  }
+
+}
 
 const {deconstruct, reconstruct} = require('../lib');
 
-describe('fparts.deconstruct()', () => {
+// deconstruct();
 
-  it('should deconstruct an anonmyous function (0)', () => {
-    var func = () => {};
-    var obj = deconstruct(func);
-    expect(obj).to.eql({
-      isGenerator: false,
-      isAsync: false,
-      name: null,
-      params: null,
-      body: null,
-    });
-  });
+// reconstruct();
 
-  it('should deconstruct an anonmyous function (1)', () => {
-    var func =  () => {console.log(1)};
-    var obj = deconstruct(func);
-    expect(obj).to.eql({
-      isGenerator: false,
-      isAsync: false,
-      name: null,
-      params: null,
-      body: 'console.log(1)',
-    });
-  });
+//
+// async
+// generator
+// params
+// body
+// name
+//
 
-  it('should deconstruct an anonmyous function (2)', () => {
-    var func = () => console.log(1);
-    var obj = deconstruct(func);
-    expect(obj).to.eql({
-      isGenerator: false,
-      isAsync: false,
-      name: null,
-      params: null,
-      body: 'console.log(1)',
-    });
-  });
+var async = [
+  '',
+  'async ',
+];
 
-  it('should deconstruct an async function (0)', () => {
-    var func = async () => console.log(1);
-    var obj = deconstruct(func);
-    expect(obj).to.eql({
-      isGenerator: false,
-      isAsync: true,
-      name: null,
-      params: null,
-      body: 'console.log(1)',
-    });
-  });
+var generator = [
+  '',
+  '*',
+];
 
-  it('should deconstruct an async function (1)', () => {
-    var func = async() => console.log(1);
-    var obj = deconstruct(func);
-    expect(obj).to.eql({
-      isGenerator: false,
-      isAsync: true,
-      name: null,
-      params: null,
-      body: 'console.log(1)',
-    });
-  });
+var name = [
+  '',
+  'a',
+  'aa',
+];
 
-  it('should deconstruct an generator function (0)', () => {
-    var func = function* () { console.log(1) };
-    var obj = deconstruct(func);
-    expect(obj).to.eql({
-      isGenerator: true,
-      isAsync: false,
-      name: null,
-      params: null,
-      body: 'console.log(1)',
-    });
-  });
+var params = [
+  '',
+  'a',
+  'a, b',
+];
 
-  it('should deconstruct an generator function (1)', () => {
-    var func = function * () { console.log(1) };
-    var obj = deconstruct(func);
-    expect(obj).to.eql({
-      isGenerator: true,
-      isAsync: false,
-      name: null,
-      params: null,
-      body: 'console.log(1)',
-    });
-  });
+var body = [
+  'return 1;',
+];
 
-  it('should deconstruct an generator function (2)', () => {
-    var func = function *() { console.log(1) };
-    var obj = deconstruct(func);
-    expect(obj).to.eql({
-      isGenerator: true,
-      isAsync: false,
-      name: null,
-      params: null,
-      body: 'console.log(1)',
-    });
-  });
 
-  it('should deconstruct an generator function (3)', () => {
-    var func = function * aa() {function aaa() {return 2;}return aaa();};
-    var obj = deconstruct(func);
-    expect(obj).to.eql({
-      isGenerator: true,
-      isAsync: false,
-      name: 'aa',
-      params: null,
-      body: 'function aaa() {return 2;}return aaa();',
-    });
-  });
 
-  it('should deconstruct a complex function (0)', async () => {
-    var func = async function (a, b = 2, c = () => 3) {
-      function aaa(a,b,c) {
-        return a + b + c;
-      }
-      return aaa(a,b,c());
-    };
 
-    var obj = deconstruct(func);
-    expect(obj).to.eql({
-      isGenerator: false,
-      isAsync: true,
-      name: null,
-      params: 'a, b = 2, c = () => 3',
-      body: 'function aaa(a,b,c) {\n        return a + b + c;\n      }\n      return aaa(a,b,c());',
+
+var x;
+
+describe('fparts', async () => {
+  var obj = new Possibilities(async, generator, name, params, body);
+
+  // obj.preprocess((item) => {
+  //   if (a === 0) return b !== 0;
+  // });
+
+  var matrix = obj.run();
+  console.log(matrix);
+
+  matrix.forEach(async item => {
+    var result, func, describeName;
+    const [isAsync, isGenerator, name, params, body] = item;
+
+    func = reconstruct({
+      isAsync,
+      isGenerator,
+      name,
+      params,
+      body,
     });
 
-    var func1 = reconstruct(obj);
-    var result = await func1(1,2,() => 3);
-    result.should.eql(6);
-  });
+    describeName = item.map(val => !val ? "''" : val).join(' ');
+    describe(`(${describeName})`, () => {
+      it ('should reconstruct correctly', async (done) => {
+        if (isGenerator) {
+          let obj = func();
+          result = obj.next().value;
+        }
+        else if (isAsync) {
+          result = await func();
+        }
+        else {
+          result = func();
+        }
+        expect(result).to.eql(1);
+        done();
+      });
 
+      obj = deconstruct(func);
+
+      it ('should deconstruct correctly', () => {
+        console.log(obj);
+        expect(obj).to.eql({
+          isGenerator,
+          isAsync,
+          name,
+          params,
+          body,
+        });
+      });
+    });
+
+  });
 });
+
+
+
+
+
+
+
+
+
+
+// // @todo tests arrow func vs regular for ctx.
+// describe('fparts.reconstruct()', () => {
+//   it('should reconstruct all function types', () => {
+//     var obj = {
+//       isGenerator: false,
+//       isAsync: false,
+//       name: null,
+//       params: null,
+//       body: 'return 1',
+//     };
+//   });
+// });
+//
+// describe('fparts.deconstruct()', () => {
+//
+//   it('should deconstruct an anonmyous function (0)', () => {
+//     var func = () => {};
+//     var obj = deconstruct(func);
+//     expect(obj).to.eql({
+//       isGenerator: false,
+//       isAsync: false,
+//       name: null,
+//       params: null,
+//       body: null,
+//     });
+//   });
+//
+//   it('should deconstruct an anonmyous function (1)', () => {
+//     var func =  () => {console.log(1)};
+//     var obj = deconstruct(func);
+//     expect(obj).to.eql({
+//       isGenerator: false,
+//       isAsync: false,
+//       name: null,
+//       params: null,
+//       body: 'console.log(1)',
+//     });
+//   });
+//
+//   it('should deconstruct an anonmyous function (2)', () => {
+//     var func = () => console.log(1);
+//     var obj = deconstruct(func);
+//     expect(obj).to.eql({
+//       isGenerator: false,
+//       isAsync: false,
+//       name: null,
+//       params: null,
+//       body: 'console.log(1)',
+//     });
+//   });
+//
+//   it('should deconstruct an async function (0)', () => {
+//     var func = async () => console.log(1);
+//     var obj = deconstruct(func);
+//     expect(obj).to.eql({
+//       isGenerator: false,
+//       isAsync: true,
+//       name: null,
+//       params: null,
+//       body: 'console.log(1)',
+//     });
+//   });
+//
+//   it('should deconstruct an async function (1)', () => {
+//     var func = async() => console.log(1);
+//     var obj = deconstruct(func);
+//     expect(obj).to.eql({
+//       isGenerator: false,
+//       isAsync: true,
+//       name: null,
+//       params: null,
+//       body: 'console.log(1)',
+//     });
+//   });
+//
+//   it('should deconstruct an generator function (0)', () => {
+//     var func = function* () { console.log(1) };
+//     var obj = deconstruct(func);
+//     expect(obj).to.eql({
+//       isGenerator: true,
+//       isAsync: false,
+//       name: null,
+//       params: null,
+//       body: 'console.log(1)',
+//     });
+//   });
+//
+//   it('should deconstruct an generator function (1)', () => {
+//     var func = function * () { console.log(1) };
+//     var obj = deconstruct(func);
+//     expect(obj).to.eql({
+//       isGenerator: true,
+//       isAsync: false,
+//       name: null,
+//       params: null,
+//       body: 'console.log(1)',
+//     });
+//   });
+//
+//   it('should deconstruct an generator function (2)', () => {
+//     var func = function *() { console.log(1) };
+//     var obj = deconstruct(func);
+//     expect(obj).to.eql({
+//       isGenerator: true,
+//       isAsync: false,
+//       name: null,
+//       params: null,
+//       body: 'console.log(1)',
+//     });
+//   });
+//
+//   it('should deconstruct an generator function (3)', () => {
+//     var func = function * aa() {function aaa() {return 2;}return aaa();};
+//     var obj = deconstruct(func);
+//     expect(obj).to.eql({
+//       isGenerator: true,
+//       isAsync: false,
+//       name: 'aa',
+//       params: null,
+//       body: 'function aaa() {return 2;}return aaa();',
+//     });
+//   });
+//
+//   it('should deconstruct a complex function (0)', async () => {
+//     var func = async function (a, b = 2, c = () => 3) {
+//       function aaa(a,b,c) {
+//         return a + b + c;
+//       }
+//       return aaa(a,b,c());
+//     };
+//
+//     var obj = deconstruct(func);
+//     expect(obj).to.eql({
+//       isGenerator: false,
+//       isAsync: true,
+//       name: null,
+//       params: 'a, b = 2, c = () => 3',
+//       body: 'function aaa(a,b,c) {\n        return a + b + c;\n      }\n      return aaa(a,b,c());',
+//     });
+//
+//     var func1 = reconstruct(obj);
+//     var result = await func1(1,2,() => 3);
+//     result.should.eql(6);
+//   });
+//
+// });
